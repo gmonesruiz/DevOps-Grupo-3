ae0b5ba35fab152346b6a1a8503291cd5621556e
Jose Dominguez <itjosedominguez@gmail.com>
2023-03-07 23:21:33 +0100

    se agrego el recurso s3 a main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,30 @@ resource "aws_instance" "ec2_instances" {
     Name = "ec2-instance-${count.index + 1}"
   }
 }
+
+############## Pruebas de AWS_S3 #######################
+
+variable "s3_bucket_name" {
+  type    = list(string)
+  default = ["mongodb", "test", "staging", "dev", "sandbox"]
+}
+
+resource "aws_s3_bucket" "bucket" {
+  count         = "${length(var.s3_bucket_name)}"
+  bucket        = "${element(var.s3_bucket_name, count.index)}"
+  acl           = "private"
+  force_destroy = "true"
+    lifecycle_rule {
+    enabled = true
+
+    transition {
+      days = 180
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days = 360
+      storage_class = "GLACIER"
+    }
+  }
+}
--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   access_key                  = "mock_access_key"
   secret_key                  = "mock_access_key"
   region                      = "us-east-1"
-  s3_force_path_style         = false
+  s3_use_path_style           = false
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
