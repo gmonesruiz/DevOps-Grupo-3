a1da4ef0a8a2f21db184e5af82be7fe96e19dbad
Gabriel MR <gabrielmonesruiz@outlook.com>
2023-03-07 12:45:26 -0300

    Actualización de archivos terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region = "us-east-1"
-  access_key = "<ACCESS_KEY>"
-  secret_key = "<SECRET_KEY>"
-}
+
 variable "region" {
   default = "us-east-1"
 }
@@ -26,25 +22,4 @@ resource "aws_instance" "ec2_instances" {
   tags = {
     Name = "ec2-instance-${count.index + 1}"
   }
-}
-
-# Crear cluster de Kubernetes
-resource "aws_eks_cluster" "kubernetes_cluster" {
-  name = "my-cluster"
-  role_arn = aws_iam_role.cluster.arn
-  vpc_config {
-    security_group_ids = [aws_security_group.kubernetes.id]
-    subnet_ids = aws_subnet.private.*.id
-  }
-}
-
-# Crear base de datos MongoDB
-resource "aws_docdb_cluster_instance" "mongodb_instances" {
-  count = 3
-  identifier = "mongodb-instance-${count.index + 1}"
-  instance_class = "db.t3.medium"
-  engine = "docdb"
-  engine_version = var.mongo_version
-  db_subnet_group_name = aws_docdb_subnet_group.name
-  db_cluster_identifier = aws_docdb_cluster.default.id
 }
