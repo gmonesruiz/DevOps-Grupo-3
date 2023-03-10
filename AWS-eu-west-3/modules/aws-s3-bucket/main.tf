399b30a4405c5900d6e6011f0aca06b6bd47dfcf
Jose Dominguez <itjosedominguez@gmail.com>
2023-03-10 20:05:11 +0100

    Se añadió la carpeta AWS-eu-west-3 y se añadió información en README.md, además se elimino el provider localstack

---
+++ AWS-eu-west-3/modules/aws-s3-bucket/main.tf
@@ -0,0 +1,59 @@
+terraform {
+#  #############################################################
+#  ## Despues de correr terraform apply,
+#  ## se puede descomentar esta parte o  copiar al main,
+#  ## de esta manera se cambia de BACKEND local a BACKEND en AWS.
+#  #############################################################
+#    backend "s3" {
+#      bucket         = "devops-bootcamp-grupo3" # REEMPLAZAR CON EL NOMBRE DEL BUCKET NECESARIO
+#      key            = "grupo-3/import-bootstrap/terraform.tfstate"
+#      region         = "eu-west-3"
+#      dynamodb_table = "terraform-state-locking"
+#      encrypt        = true
+#    }
+#
+
+#  ## Se crean los recursos para poder guardar el state de terraform
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.7"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "devops-bootcamp-grupo3" # REEMPLAZAR CON EL NOMBRE DEL BUCKET NECESARIO
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket        = aws_s3_bucket.terraform_state.bucket 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DevOps-Grupo-3
 Trabajo integrador Bootcamp DevOps Engineer Educación IT
+
+** Para poder usar el backend remoto en AWS, primero se deben crear los recursos. **
+
+Ingresar a aws-backend y ejecutar terraform init, plan y apply
+
+	$ cd aws-backend
+	$ aws-backend/ terraform init
+	$ aws-backend/ terraform plan
+	$ aws-backend/ terraform apply
+
+
+
